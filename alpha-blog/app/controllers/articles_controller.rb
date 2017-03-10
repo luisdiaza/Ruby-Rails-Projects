2d172fc459b601914d9e834ff9bd4ba46ff7af75
Luis Diaz <diazluis1048@gmail.com>
2017-03-10 11:23:37 -0500

    Extracted redundancies and prepared for production

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]#reduces redundancy by calling set article method in specified methods
+
   def index
     @articles = Article.all #grabs all articles from database
   end
@@ -6,7 +8,6 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def edit
-    @article = Article.find(params[:id])#creates an articles instance variable that finds the articles
   end
   def create
     @article = Article.new(article_params)
@@ -18,7 +19,6 @@ class ArticlesController < ApplicationController
     end
   end
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -28,15 +28,16 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :description)
     end
