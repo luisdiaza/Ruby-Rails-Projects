ef72cf6f0c0b219165b789b7702a8c6942ec0c5e
Luis Diaz <diazluis1048@gmail.com>
2017-03-16 23:19:57 -0400

    styled flash messages

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render "new"
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
   end
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render "edit"
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
   end
   def destroy
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"#danger gives you a red background
     redirect_to articles_path
   end
   private
