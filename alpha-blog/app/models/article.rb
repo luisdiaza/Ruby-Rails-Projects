67fe941f2aa9418be59feb19d8f8fc051a23e6d6
Luis Diaz <diazluis1048@gmail.com>
2017-04-03 14:35:23 -0400

    add association between user and articles

--- alpha-blog/app/models/article.rb
+++ alpha-blog/app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 3, maximum: 50 } #makes it so articles need titles to be entered in database
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
-
+  validates :user_id, presence: true
 end
